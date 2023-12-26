3785f24b3e624df4573a3a8eef027c6ad710db35
Sualeh Fatehi <sualeh@hotmail.com>
2023-12-26 08:33:56 -0500

    Update copyright year

--- src/main/java/us/fatehi/magnetictrack/Track3.java
+++ src/main/java/us/fatehi/magnetictrack/Track3.java
@@ -2,7 +2,7 @@
  *
  * Magnetic Track Parser
  * https://github.com/sualeh/magnetictrackparser
- * Copyright (c) 2014-2023, Sualeh Fatehi.
+ * Copyright (c) 2014-2024, Sualeh Fatehi.
  *
  */
 package us.fatehi.magnetictrack;
